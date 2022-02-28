c71cf7e91445c40d349f071acb5c48b116a4c77c
alinaraz <100436198+alinaraz@users.noreply.github.com>
2022-02-28 12:22:39 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int number = 4;
+        int value = 6;
+        System.out.println ("Addition of number and value is: " + (number + value));
+        System.out.println ("Subtraction of number and value is: " + (number - value));
+        System.out.println ("Multiplication of number and value is: " + (number * value));
+        System.out.println ("Division of number and value is: " + (number / value));
+        System.out.println ("Modulus of number and value is: " + (number % value));
+
+
+        System.out.println("Hello world");
+
+    }
+}
